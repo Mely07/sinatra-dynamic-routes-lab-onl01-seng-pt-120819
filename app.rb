bd016676491c100415bfbdff69ae5e3c76a3a3f3
Damely Tineo <damely.tineo07@gmail.com>
2020-03-18 04:16:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,12 +4,12 @@ class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do
     @name = params[:name].reverse
-    "#{@name}"
+    @name
   end
   
   get '/square/:number'do
     @number = params[:number].to_i ** 2
-    "#{@number}"
+    @number
   end
   
   get '/say/:number/:phrase' do
